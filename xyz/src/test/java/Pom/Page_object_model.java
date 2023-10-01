781ff39e2b0192904593d786b878ae64f7b804bd
lenovo <lenovo@DESKTOP-OUBE3OH>
2023-10-01 00:33:25 -0700

    This is My First code Push

---
+++ xyz/src/test/java/Pom/Page_object_model.java
@@ -0,0 +1,9 @@
+package Pom;
+
+public class Page_object_model {
+	
+	public void second()
+	{
+		// this is for testing the push code from workspace to github
+	}
+}
---
+++ xyz/src/test/java/Stepdefination/Setp.java
@@ -0,0 +1,10 @@
+package Stepdefination;
+
+public class Setp {
+	
+	
+	public void fristtest()
+	{
+		// this is for testing the push code from workspace to github
+	}
+}
